c7416609eb991bac585674d7e3525bb413c57590
genedan <genedan@gmail.com>
2020-06-26 21:38:31 -0500

    sqlite deployment script

---
+++ tests/deploy_sqlite.py
@@ -0,0 +1,15 @@
+import os
+import sqlalchemy as sa
+
+from sqlalchemy.orm import sessionmaker
+
+from party import Base
+
+engine = sa.create_engine(
+            'sqlite:///pcdm.db',
+            echo=True
+        )
+session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
+
+os.remove('pcdm.db')
